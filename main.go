863934346cb379564c1a35d4ab080e3c4c08d91c
Deven Rathod <devenrathod2001@gmail.com>
2022-02-28 17:35:09 +0530

    :zap: Simple Go Server GET & POST Request

--- main.go
+++ main.go
@@ -1,13 +1,39 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"html"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
 
 func main() {
+
+	requestBody, err := json.Marshal(map[string]string{
+		"name":  "John",
+		"email": "john@example.com",
+	})
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	resp, err := http.Post("https://httpbin.org/post", "application/json", bytes.NewBuffer(requestBody))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Println(string(body))
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "public/index.html")
