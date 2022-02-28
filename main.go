0acb58d240222edd5e8f57fce26c3985c2eba44b
Deven Rathod <devenrathod2001@gmail.com>
2022-02-28 17:00:59 +0530

    Simple Go Server Created

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "public/index.html")
+	})
+
+	http.HandleFunc("/name/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
+
+	log.Fatal(http.ListenAndServe(":8081", nil))
+
+}
